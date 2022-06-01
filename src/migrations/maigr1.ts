be6409a12416b567c5514e868a4d260b54cf94f5
Andrey Demchuk <kariandreyka@gmail.com>
2022-06-01 18:40:45 +0300

    Fix migration

--- src/migrations/maigr1.ts
+++ src/migrations/maigr1.ts
@@ -3,7 +3,6 @@ import { storage } from "../services/userPreferences";
 export default {
   name: "test-migration",
   up: () => {
-    storage.clear();
     storage.setItem("biba", ["biba", "boba", "aboba"]);
   }
 };
